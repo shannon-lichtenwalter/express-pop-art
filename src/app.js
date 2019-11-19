66511f16bd390e6d90578641948b995f02a08ae2
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2019-11-18 20:06:13 -0700

    successfully interacting with express serverfor basic get request

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const events = require('../events.json');
 
 const app = express();
 
@@ -13,9 +14,19 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use(validateBearerToken);
 
-app.get('/', (req,res) => {
-  return res.send('Hello, world!');
+function validateBearerToken(req,res,next){
+  const auth = req.get('Authorization');
+  const apiToken = process.env.API_TOKEN;
+  if(!auth || auth.split(' ')[1] !== apiToken){
+    return res.status(401).json({error: 'Unauthorized request'});
+  }
+  next();
+}
+
+app.get('/api/events', (req,res,next) => {
+  return res.status(200).json(events);
 });
 
 app.use(function errorHandler(error, req, res, next ){
